1bc16521f7eff14128f65bb01cab77b2055d8183
Ben Mason <ben@sharkbyte.co.uk>
2015-02-23 10:52:48 +0000

    The map now appears.

--- scout/static/js/app.js
+++ scout/static/js/app.js
@@ -2,9 +2,17 @@
 
 	'use strict';
 
-	angular.module('scout', []).run(function () {
-		console.log('Scout application module running...');
-	});
+	angular.module('scout', ['ui.map'])
+		.run(function () {
+			console.log('Scout application module running...');
+		}).controller('MapCtrl', ['$scope', function($scope) {
+			$scope.mapOptions = {
+				center: new google.maps.LatLng(54.5104237, -2.6434287),
+				mapTypeControl: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				zoom: 6
+			};
+		}]);
 
 	window.onGoogleReady = function () {
 		angular.bootstrap(document.body, ['scout']);
