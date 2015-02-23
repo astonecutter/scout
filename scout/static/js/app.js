03b507d95998c07f9b586d55a1db518e0c84517a
Ben Mason <ben@sharkbyte.co.uk>
2015-02-23 15:58:39 +0000

    Update the app to use angular for the map.

--- scout/static/js/app.js
+++ scout/static/js/app.js
@@ -2,16 +2,48 @@
 
 	'use strict';
 
-	angular.module('scout', ['ui.map'])
+	scout.app = angular.module('scout', ['ui.map'])
 		.run(function () {
 			console.log('Scout application module running...');
 		}).controller('MapCtrl', ['$scope', function($scope) {
+
+			var geocoder = new google.maps.Geocoder(),
+				bounds = new google.maps.LatLngBounds();
+
+			$scope.myMarkers = [];
+
 			$scope.mapOptions = {
 				center: new google.maps.LatLng(54.5104237, -2.6434287),
 				mapTypeControl: false,
-				mapTypeId: google.maps.MapTypeId.ROADMAP,
-				zoom: 6
+				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
+
+			$scope.openMarkerInfo = function(marker) {
+				$scope.currentMarker = marker;
+				$scope.currentMarkerLat = marker.getPosition().lat();
+				$scope.currentMarkerLng = marker.getPosition().lng();
+				$scope.myInfoWindow.open($scope.myMap, marker);
+			};
+
+			// load each of the existing markers
+			angular.forEach(scout.markers, function (marker) {
+				if (marker.address) {
+					geocoder.geocode({'address': marker.address}, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+							var marker = new google.maps.Marker({
+                                map: $scope.myMap,
+                                position: results[0].geometry.location
+                            });
+                            $scope.myMarkers.push(marker);
+							bounds.extend(marker.position);
+							$scope.myMap.fitBounds(bounds);
+                        } else {
+                            console.log("Geocode was not successful for the following reason: " + status);
+                        }
+                    });
+				}
+			});
+
 		}]);
 
 	window.onGoogleReady = function () {
